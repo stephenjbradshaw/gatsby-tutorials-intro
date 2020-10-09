9116de03e9af54af0928dae898d3990bf20236f7
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-09 12:46:57 +0100

    Add another page, demonstrating automatic routing

---
+++ src/pages/about.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Cool how the routing is done under the hood.</p>
+    </div>
+  )
+}
