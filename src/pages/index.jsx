972a2b40a3fc129071d744498c426d5cca72e6e1
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-09 12:43:56 +0100

    Add a component

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <>
+      <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </>
+  )
+}
