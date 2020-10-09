213bfb070b37b90f33fb3c55e0babfa59a6b773d
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-09 12:51:01 +0100

    Create re-usable header component

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import Header from "../components/header"
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
+      <Header />
       <p>Cool how the routing is done under the hood.</p>
     </div>
   )
