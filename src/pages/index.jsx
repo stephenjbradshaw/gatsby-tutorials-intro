813ed964c4edb6f39a855be68c625b99d2bb2fb5
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-09 13:21:09 +0100

    Linking between pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <>
-      <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <p>Hello Gatsby!</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </>
+    </div>
   )
 }
